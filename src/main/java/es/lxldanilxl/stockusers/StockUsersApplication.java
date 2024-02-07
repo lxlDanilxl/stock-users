23d4880823379ce2ff1f8b9ce42ac3b487d94953
Rogelio Daniel Franco Martínez <contacto@lxldanilxl.es>
2024-02-07 16:20:41 +0100

    Initial commit

---
+++ src/main/java/es/lxldanilxl/stockusers/StockUsersApplication.java
@@ -0,0 +1,13 @@
+package es.lxldanilxl.stockusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/lxldanilxl/stockusers/StockUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package es.lxldanilxl.stockusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
